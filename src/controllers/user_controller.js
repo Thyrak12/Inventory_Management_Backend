abdf52ec4cf0114599f39f0fa2cddf183437cc5c
Thyrak12 <thyrak9167@gmail.com>
2025-07-25 17:27:50 +0700

    goo

--- src/controllers/user_controller.js
+++ src/controllers/user_controller.js
@@ -28,6 +28,10 @@ import jwt from 'jsonwebtoken';
  *               email:
  *                 type: string
  *                 format: email
+ *               name:
+ *                 type: string
+ *               role:
+ *                 type: string
  *               password:
  *                 type: string
  *                 format: password
@@ -38,14 +42,14 @@ import jwt from 'jsonwebtoken';
  *         description: Email already exists
  */
 export const registerUser = async (req, res) => {
-    const { email, password } = req.body;
+    const { email, name, password, role } = req.body;
 
     try {
         const exists = await db.User.findOne({ where: { email } });
         if (exists) return res.status(400).json({ error: 'Email already registered' });
 
-        const user = await db.User.create({ email, password });
-        res.status(201).json({ message: 'User registered', user: { id: user.id, email: user.email } });
+        const user = await db.User.create({ email, name, password , role});
+        res.status(201).json({ message: 'User registered', user: { id: user.id, email: user.email, role: user.role } });
     } catch (err) {
         res.status(500).json({ error: 'Registration error', details: err.message });
     }
@@ -94,7 +98,7 @@ export const loginUser = async (req, res) => {
             expiresIn: '1d',
         });
 
-        res.json({ token, user: { id: user.id, email: user.email } });
+        res.json({ token, user: { id: user.id, email: user.email , name: user.name , role: user.role} });
     } catch (err) {
         res.status(500).json({ error: 'Login error', details: err.message });
     }
