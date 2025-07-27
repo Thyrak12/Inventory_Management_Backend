510e2db888e2a300a74639b270ea880fa99e2bd1
Thyrak12 <thyrak9167@gmail.com>
2025-07-27 18:06:35 +0700

    add categories

--- src/routes/product_route.js
+++ src/routes/product_route.js
@@ -3,6 +3,7 @@ import { Router } from "express";
 const productRoutes = (controller) => {
     const routes = Router();
     routes.get("/", controller.getAllProducts);
+    routes.get('/categories', controller.getProductCategories);
     routes.get("/:id", controller.getProductById);
     routes.post("/", controller.createProduct);
     routes.put("/:id", controller.updateProduct);
